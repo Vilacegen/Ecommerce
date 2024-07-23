2f7a02f00d78efc8aac11efad4db842d39c61458
Vilacegen <solaceacegen@gmail.com>
2024-07-23 15:35:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+The modified Ecommerce project from the cloned repo
+I'll add onto new features
+Create an Admin Dashboard as well
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>New</header>
+    </>
+  );
+}
+
+export default App;
