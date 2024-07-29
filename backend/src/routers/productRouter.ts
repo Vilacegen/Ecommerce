41ec542961918881982042f2377456a9bbc66385
Vilacegen <solaceacegen@gmail.com>
2024-07-29 19:24:12 +0300

    New

--- backend/src/routers/productRouter.ts
+++ backend/src/routers/productRouter.ts
@@ -3,23 +3,40 @@ import asyncHandler from "express-async-handler";
 import { ProductModel } from "../models/productModel";
 
 export const productRouter = express.Router();
-// /api/prodcuts
+
+// /api/products
 productRouter.get(
   "/",
   asyncHandler(async (req, res) => {
-    const products = await ProductModel.find();
-    res.json(products);
+    console.log("Received request for all products");
+    try {
+      const products = await ProductModel.find();
+      console.log("Products fetched:", products);
+      res.json(products);
+    } catch (err) {
+      console.error("Error fetching products:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   })
 );
-// /api/slug/tshirt
+
+// /api/slug/:slug
 productRouter.get(
   "/slug/:slug",
   asyncHandler(async (req, res) => {
-    const product = await ProductModel.findOne({ slug: req.params.slug });
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: "Product Not Found" });
+    console.log(`Received request for product with slug: ${req.params.slug}`);
+    try {
+      const product = await ProductModel.findOne({ slug: req.params.slug });
+      if (product) {
+        console.log("Product found:", product);
+        res.json(product);
+      } else {
+        console.log("Product not found");
+        res.status(404).json({ message: "Product Not Found" });
+      }
+    } catch (err) {
+      console.error("Error fetching product:", err);
+      res.status(500).json({ message: "Internal Server Error" });
     }
   })
 );
